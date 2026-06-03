2bb7510be8f27536a8e3f3ce129835b209d39519
buildwithvishal <vishallakshya2004@gmail.com>
2026-06-03 08:00:23 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 25685
+let accountMail = "vishal@chaudhary"
+var accountPass = "128564"
+accountCity = "Bengalore"
+
+// accountId = 2;
+accountMail = "lakshya@chaudhary"
+
+accountPass = 256423
+accountCity = 'Bharatpur'
+let accountstate;
+
+console.log(accountPass)
+console.log([accountId, accountMail, accountPass, accountCity, accountstate]);
+
+// prefer not to use var and without declaring the variable type 
+// always use let and const, let is changeable while const is unchangeable
+// var don't have block scope while let have!!
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,17 @@
+"use strict" // treat all js code as newer version
+
+//alert(3+3) // nodejs mei nahi chalega browser mei chal jayega
+// code readability should be high 
+
+//nuber => 2 to pwr 53
+//bigint
+//strin => ""
+//boolean ==> truee/false
+//null => standalone value(itentional absence dikhane ke liye use hota hai)
+//undefined => abhi value assign nahi huyi hai 
+//symbol => unique
+
+//object
+
+console.log(typeof null); // object
+console.log(typeof undefined) // undefined
