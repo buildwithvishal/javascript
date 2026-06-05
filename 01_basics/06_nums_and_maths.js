20046dee813ecbb5a5eafeeb581ad9a9b0374e79
buildwithvishal <vishallakshya2004@gmail.com>
2026-06-05 07:46:09 +0000

    maths

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,12 @@
+console.log("hello world")
+
+// console.log(Math)
+
+// console.log(Math.random())  //always between 0 and 1
+
+// console.log(Math.random()*10+1)
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random()*(max-min+1))+min)
