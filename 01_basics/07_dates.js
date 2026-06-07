c9e238b7ed575837e723796a53b74e2e7819cb5d
buildwithvishal <vishallakshya2004@gmail.com>
2026-06-07 17:44:13 +0000

    dates

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,15 @@
+let myDate = new Date()
+
+// console.log(myDate)
+// console.log(myDate.toDateString())
+// console.log(myDate.toString())
+// console.log(myDate.toISOString())
+// console.log(myDate.toJSON())
+// console.log(myDate.toLocaleDateString())
+// console.log(myDate.toLocaleTimeString())
+// console.log(myDate.toTimeString())
+
+console.log(typeof(myDate))
+
+let mycreateddate = new Date(2026, 4, 3)
+console.log(mycreateddate.toDateString())
