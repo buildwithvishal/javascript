b8a2327fbe60c5a9baa993516cbeb09646f000c8
buildwithvishal <vishallakshya2004@gmail.com>
2026-06-15 07:15:34 +0000

    destructure

---
+++ 02_basics/05_objects.js
@@ -0,0 +1,8 @@
+const course = {
+    courseName : "js with code or chai",
+    price : "999",
+    courseInstructor : "hitesh"
+}
+
+const {courseInstructor: Instructor} = course;
+console.log(Instructor);
