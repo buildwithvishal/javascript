482295a54691242bfba2af5e71b1fff4f81cf958
buildwithvishal <vishallakshya2004@gmail.com>
2026-06-15 07:44:21 +0000

    functions1

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,36 @@
+function myName(){
+    console.log("vishal")
+}
+
+// myName()
+
+// function addTwoNumbers(number1, number2){
+//     console.log(number1 + number2)
+// }
+
+// addTwoNumbers(3, 4);   // 7
+// addTwoNumbers(3, "4"); // 34
+// addTwoNumbers(3, "a"); // 3a
+// addTwoNumbers(3, null) // 3
+
+function addTwoNumbers(number1, number2){
+    let result = number1 + number2;
+    return result;
+}
+ 
+
+// const result = addTwoNumbers(3, 5)
+// console.log("Result: ", result)
+
+
+function loginUserMessage(username = 'sam'){
+    if(!username){
+        console.log("please enter a username");
+        return;
+    }
+
+    return `${username} just logged in`
+}
+
+console.log(loginUserMessage())
+ 
