5c314073e817ff76bac77526f7380e26ad8d0839
buildwithvishal <vishallakshya2004@gmail.com>
2026-06-29 11:29:51 +0000

    loops

---
+++ 05_iterations/two.js
@@ -0,0 +1,12 @@
+let i =0;
+while(i<=10){
+    console.log(`value of index is ${i}`);
+    i = i+2;
+}
+
+let score = 1;
+do{
+    console.log(`score is ${score}`)
+    score++;
+}
+while(score<=10)
